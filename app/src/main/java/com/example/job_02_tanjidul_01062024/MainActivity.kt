3a4738dd667ba46d8a51b884c2aead571758e884
tomu03 <369tomu@gamil.com>
2024-06-01 12:21:13 +0600

    model and adapter add

--- app/src/main/java/com/example/job_02_tanjidul_01062024/MainActivity.kt
+++ app/src/main/java/com/example/job_02_tanjidul_01062024/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.job_02_tanjidul_01062024
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.job_02_tanjidul_01062024.Activity.ProfileListActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
+
+    private lateinit var updateProfileBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        listButton = findViewById(R.id.profileListBtn)
+
+        listButton.setOnClickListener {
+            startActivity(Intent(this, ProfileListActivity::class.java))
+            finish()
         }
     }
 }
---
+++ app/src/main/java/com/example/job_02_tanjidul_01062024/Repository/UserProfileRepository.kt
@@ -0,0 +1,23 @@
+package com.example.job_02_tanjidul_01062024.Repository
+
+import UserProfile
+import androidx.lifecycle.LiveData
+import com.example.job_02_tanjidul_01062024.Database.UserProfileDao
+
+class UserProfileRepository(private val userProfileDao: UserProfileDao) {
+    fun getUserProfiles(): LiveData<List<UserProfile>> {
+        return userProfileDao.getAllUserProfiles()
+    }
+
+    suspend fun insert(userProfile: UserProfile) {
+        userProfileDao.insert(userProfile)
+    }
+
+    suspend fun update(userProfile: UserProfile) {
+        userProfileDao.update(userProfile)
+    }
+
+    suspend fun delete(userProfile: UserProfile) {
+        userProfileDao.delete(userProfile)
+    }
+}
